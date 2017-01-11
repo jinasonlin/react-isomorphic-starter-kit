d8e30385a81774cd5f6da159c4ec4751379060fd
Lin Qun <jinasonlin@gmail.com>
2017-01-11 13:47:12 +0800

    optimize redux dev tool

--- client/modules/redux/index.js
+++ client/modules/redux/index.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { browserHistory, Router } from 'react-router';
+import { configureStore, DevTools } from './store'
 import routes from './route';
-import reducers from './reducers'
 
 let initState;
 if (window.__data) {
   initState = window.__data;
 }
-let store = createStore(reducers, initState)
+const store = configureStore(initState);
 
-let rootElement = document.getElementById('app')
+const rootElement = document.getElementById('app')
 render(
   <Provider store={store}>
     <Router routes={routes} history={browserHistory} />
   </Provider>,
   rootElement
 )
+
+if (DevTools) {
+  const devtools = document.getElementById('devtools')
+  render(
+    <Provider store={store}>
+      <DevTools/>
+    </Provider>,
+    devtools
+  )
+}
--- client/modules/reduxPlus/index.js
+++ client/modules/reduxPlus/index.js
@@ -10,10 +10,10 @@ let initState;
 if (window.__data) {
   initState = window.__data;
 }
-let store = configureStore(browserHistory, initState)
+const store = configureStore(browserHistory, initState)
 const history = syncHistoryWithStore(browserHistory, store)
 
-let rootElement = document.getElementById('app')
+const rootElement = document.getElementById('app')
 render(
   <Provider store={store}>
     <Router routes={routes} history={history} />
@@ -21,8 +21,8 @@ render(
   rootElement
 )
 
-if (__DEVELOPMENT__ && __DEVTOOLS__) {
-  let devtools = document.getElementById('devtools')
+if (DevTools) {
+  const devtools = document.getElementById('devtools')
   render(
     <Provider store={store}>
       <DevTools/>

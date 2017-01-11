11034c2e9a940260c46dc5e19d7f2a7668d15b33
Lin Qun <jinasonlin@gmail.com>
2017-01-11 11:50:34 +0800

    ignore

--- server/config/routes/m.js
+++
@@ -1,9 +0,0 @@
-import { serverRender, pageRender } from '../render';
-
-export default function (app) {
-  app.get('(/m|/m/*)', (...args) => {
-    pageRender({
-      page: 'm'
-    }, ...args);
-  });
-}

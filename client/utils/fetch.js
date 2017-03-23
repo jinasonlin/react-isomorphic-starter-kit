ed82fc08571328a5e87143f7c09f31dcb0907df1
Lin Qun <jinasonlin@gmail.com>
2017-03-23 11:56:25 +0800

    fixed import error

--- client/utils/fetch.js
+++ client/utils/fetch.js
@@ -1,6 +1,6 @@
-import { setServices } from 'fetch-api';
 import { API } from 'config';
+import { setServices } from './fetchAPI';
 
 setServices(API);
 
-export { fetchAPI, getURL } from 'fetch-api';
+export { fetchAPI, getURL } from './fetchAPI';

26695d2798492bd13473c6248e544013c7cd599c
Lin Qun <jinasonlin@gmail.com>
2017-03-28 18:11:48 +0800

    remove redux-devtools

--- client/modules/redux/index.js
+++ client/modules/redux/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { browserHistory, Router } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { configureStore, DevTools, DevToolsInner } from './redux/store';
+import configureStore from './redux/store';
 import routes from './route';
 
 let initState;
@@ -20,18 +20,3 @@ render(
   </Provider>,
   rootElement
 );
-
-/**
- * TODO
- * 考虑移除redux-devtools依赖。开发时，使用官方提供的浏览器插件。
- */
-
-if (DevTools && DevToolsInner) {
-  const devtools = document.getElementById('devtools');
-  render(
-    <Provider store={store}>
-      <DevTools />
-    </Provider>,
-    devtools
-  );
-}
--- client/modules/redux/redux/store.js
+++ client/modules/redux/redux/store.js
@@ -1,38 +1,15 @@
-import React from 'react';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import thunkMiddleware from 'redux-thunk';
-import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
-import DockMonitor from 'redux-devtools-dock-monitor';
+import reducers from './reducers';
 
-function getDevTools() {
-  let DevTools;
-  let DevToolsInstrument;
-  let DevToolsInner = true;
-  if (__DEVELOPMENT__ && __DEVTOOLS__) {
-    DevTools = createDevTools(
-      <DockMonitor defaultIsVisible={false} toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
-        <LogMonitor theme="tomorrow" preserveScrollTop={false} />
-      </DockMonitor>
-    );
-  }
-  if (DevTools) {
-    if (__CLIENT__ && window.devToolsExtension) {
-      DevToolsInstrument = window.devToolsExtension();
-      DevToolsInner = false;
-    } else {
-      DevToolsInstrument = DevTools.instrument();
-    }
-  }
-  return { DevTools, DevToolsInstrument, DevToolsInner };
+let DevToolsInstrument;
+
+if (__CLIENT__ && window.devToolsExtension) {
+  DevToolsInstrument = window.devToolsExtension();
 }
 
-const { DevTools, DevToolsInstrument, DevToolsInner } = getDevTools();
-
-export { DevTools, DevToolsInner };
-
-export function configureStore(history, initialState) {
+export default (history, initialState) => {
   const reduxRouterMiddleware = routerMiddleware(history);
 
   const middleware = [reduxRouterMiddleware, thunkMiddleware];
@@ -44,14 +21,13 @@ export function configureStore(history, initialState) {
     finalCreateStore = applyMiddleware(...middleware)(createStore);
   }
 
-  const reducers = require('./reducers');
   const store = finalCreateStore(reducers, initialState);
 
-  if (__DEVELOPMENT__ && module.hot) {
+  if (module.hot) {
     module.hot.accept('./reducers', () => {
-      store.replaceReducer(require('./reducers'));
+      store.replaceReducer(reducers);
     });
   }
 
   return store;
-}
+};
